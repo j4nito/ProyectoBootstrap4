12b9d4f31ca7a536f4076f7031ee989c99d156b0
= <=>
2017-01-17 19:35:31 -0500

    Codificano la página inicial

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>NETWORK | Soluciones móviles para empresas</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Cargando fuentes-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i" rel="stylesheet">
+
+    <!--Cargando íconos-->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Cargar galeria de imagenes-->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!--Carga de archivos css-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+
+  </head>
+  <body>
+    <section class="bienvenidos">
+        <header class="encabezado navbar-fixed-top" rol="banner">
+            <div class="container">
+                <a href="index.php" class="logo">
+                    <img src="images/logo.svg" alt="Logo del sitio">
+                </a>
+                <button type="button" class="boton-buscar" data-toggle="collapse" data-target="#bloque-buscar" aria-expanded="false">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+                <button type="button" class="boton-menu" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </button>
+                <!--formulario buscar-->
+                <form action="#" id="bloque-buscar" class="collapse">
+                    <div class="contenedor-bloque-buscar">
+                        <input type="text" placeholder="Buscar...">
+                        <input type="submit" value="Buscar">
+                    </div>
+                </form>
+                <!--fin formulario-->
+                <nav id="menu-principal" class="collapse">
+                    <ul>
+                        <li class="active"><a href="index.php">Inicio</a></li>
+                        <li><a href="nosotros.php">Nosotros</a></li>
+                        <li><a href="servicios.php">Servicios</a></li>
+                        <li><a href="contacto.php">Contacto</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <div class="texto-encabezado text-xs-center">
+            <div class="container">
+                <h1 class="display-4">Diseño de APPS para empresas</h1>
+                <p>Creamos aplicaciones según las necesidades y requerimientos de nuestros clientes.</p>
+                <a href="contacto.php" class="btn btn-primary btn-lg">Ponte en contacto</a>
+            </div>
+        </div>
+        <div class="flecha-bajar text-xs-center">
+            <a href="#agencia">
+                <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </a>
+        </div>
+    </section>
+    <section class="agencia p-y-1" id="agencia">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-md-8 col-xl-9">
+                    <h2 class="h3 text-xs-center font-weight-bold text-md-left">Agencia de programación de aplicaciones</h2>
+                    <p>NETWORK te asesora en el desarrollo y el diseño para que tu Aplicación Movil salga a la luz. Ponemos a tu disposición las mejores plataformas del mundo de las telecomunicaciones.</p>
+                    <p>Ponemos a tu alcance un servicio adaptado a tus necesidades, desde una consultoría integral con supervisión de todos y cada uno de los pasos que has de seguir para crear un negocio actual (comunicación, diseño corporativo, tecnologías...) a productos adaptados a metodologías ágiles de creación de apps diseñadas por nosotros y avaladas por nuestros años de experiencia.</p>
+                </div>
+                <div class="col-xs-12 col-md-4 col-xl-3">
+                    <img src="images/agencia.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="tu-mejor-eleccion p-y-1 ">
+        <div class="container">
+            <h2 class="h3 text-xs-center font-weight-bold">¿Porque somos <span>tu mejor elección?</span></h2>
+            <p class="text-xs-center">Desarrollo web y Apps para empresas y profesionales</p>
+            <div class="row">
+                <ul class="col-xs-6  col-lg-4 text-xs-center text-lg-left">
+                    <li>
+                        <i class="fa fa-briefcase" aria-hidden="true"></i>
+                        <div class="contenedor-eleccion">
+                            <h4>Aplicaciones para la nube</h4>
+                            <p class="hidden-md-down">Para empresa que requiere ampliar su plataforma de gestión.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <i class="fa fa-bullhorn" aria-hidden="true"></i>
+                        <div class="contenedor-eleccion">
+                            <h4>Fábrica de software</h4>
+                            <p class="hidden-md-down">Servicios de mejora correctiva y evolutiva para sus aplicaciones.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <i class="fa fa-comment-o" aria-hidden="true"></i>
+                        <div class="contenedor-eleccion">
+                            <h4>Gestión en la nube</h4>
+                            <p class="hidden-md-down">Para empresa que requiera ampliar sus plataformas de gestión.</p>
+                        </div>
+                    </li>
+                </ul>
+                <div class="hidden-md-down  col-lg-4">
+                    <img src="images/mundo.svg" alt="mundo móvil">
+                </div>
+                <ul class="col-xs-6 col-lg-4  text-xs-center text-lg-right">
+                    <li>
+                        <i class="fa fa-calendar" aria-hidden="true"></i>
+                        <div class="contenedor-eleccion">
+                            <h4>Videojuegos</h4>
+                            <p class="hidden-md-down">Para empresa que requiere ampliar su plataforma de gestión.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <i class="fa fa-check-square-o" aria-hidden="true"></i>
+                        <div class="contenedor-eleccion">
+                            <h4>Diseño personalizado</h4>
+                            <p class="hidden-md-down">Servicios de mejora correctiva y evolutiva para sus aplicaciones.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <i class="fa fa-cogs" aria-hidden="true"></i>
+                        <div class="contenedor-eleccion">
+                            <h4>Funciones a medida</h4>
+                            <p class="hidden-md-down">Para empresa que requiere ampliar su plataforma de gestión.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <main class="servicios p-y-1">
+        <div class="container">
+            <h2 class="text-xs-center font-weight-bold">Nuestros Servicios</h2>
+                <div class="row">
+                    <article class="col-md-4">
+                        <img src="images/servicio-1.svg" alt="Servicio Desarrollo de Aplicaciones Móbiles">
+                        <h3><a href="#">Desarrollo de Aplicaciones Móviles</a></h3>
+                        <p class="hidden-sm-down">Desarrollamos aplicaciones móviles y plataformas wev.Nuestra principal cualidad esta en el desarrollo de software a medida de tus necesidades.</p>
+                        <a href="#" class="btn btn-secondary hidden-sm-down">Más información</a>
+                    </article>
+                    <article class="col-md-4">
+                        <img src="images/servicio-2.svg" alt="Servicio Consultoría Tecnológica">
+                        <h3><a href="#">consultoría Tecnológica</a></h3>
+                        <p class="hidden-sm-down">Desarrollamos aplicaciones móviles y plataformas wev.Nuestra principal cualidad esta en el desarrollo de software a medida de tus necesidades.</p>
+                        <a href="#" class="btn btn-secondary hidden-sm-down">Más información</a>
+                    </article>
+                    <article class="col-md-4">
+                        <img src="images/servicio-3.svg" alt="Servicio Marketing y publicidad Móvil">
+                        <h3><a href="#">Marketing y Publicidad Móvil</a></h3>
+                        <p class="hidden-sm-down">Desarrollamos aplicaciones móviles y plataformas wev.Nuestra principal cualidad esta en el desarrollo de software a medida de tus necesidades.</p>
+                        <a href="#" class="btn btn-secondary hidden-sm-down">Más información</a>
+                    </article>
+                </div>
+        </div>
+    </main> 
+    <section class="ultimos-proyectos p-y-1">
+        <div class="container">
+            <h2 class="text-xs-center font-weight-bold">Últimos Proyectos</h2>
+            <div class="owl-carousel">
+                <a href="#">
+                    <h4>App Lima Travels</h4>
+                    <img src="images/proyecto1.jpg" alt="Proyecto App Lima Travels">
+                </a>
+                
+                <a href="#">
+                    <h4>App Uber</h4>
+                    <img src="images/proyecto2.jpg" alt="Proyecto App Uber">
+                </a>
+                
+                <a href="#">
+                    <h4>App Pizza Perú</h4>
+                    <img src="images/proyecto3.jpg" alt="Proyecto App Pizza Perú">
+                </a>
+                
+                <a href="#">
+                    <h4>App Muebles Hoy</h4>
+                    <img src="images/proyecto4.jpg" alt="Proyecto App  Muebles Hoy">
+                </a>
+                
+                <a href="#">
+                    <h4>App Perú</h4>
+                    <img src="images/proyecto5.jpg" alt="Proyecto App Perú">
+                </a>
+                
+                <a href="#">
+                    <h4>App Janito</h4>
+                    <img src="images/proyecto6.jpg" alt="Proyecto App Janito">
+                </a>
+            </div>
+        </div>
+    </section>
+    <footer class="piedepagina p-y-1 text-xs-center" role="contentinfo">
+        <div class="container">
+            <p>2016 &copy; NETWORK Todos los derechos reservados</p>
+            <ul class="redes-sociales">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </footer>
+    <a href="#" class="ir-arriba">
+        <i class="fa fa-arrow-circle-up" aria-hidden="true"></i>
+    </a>
+    <!--Carga de archivos js-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script type="text/javascript">
+
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: true,
+            autoWidth: false,
+
+            navText: ['<i class="fa fa-arrow-circle-left" title="Anterior"></i>', '<i class="fa  fa-arrow-circle-right" title="Siguiente"></i>'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                500: {
+                    items: 2,
+                    margin: 20
+                },
+                800: {
+                    items: 3,
+                    margin: 20
+                },
+                1000: {
+                    items: 4,
+                    margin: 20
+                }
+            }
+        });
+    });
+          </script>
+  </body>
+</html>
