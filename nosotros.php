b08c49bfd80cadb4f84d04763613b97268cf1e3e
= <=>
2017-01-18 20:38:49 -0500

    Codificando la página interna nosotros

---
+++ nosotros.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Nosotros - NETWORK | Soluciones móviles para empresas</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Cargando fuentes-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i" rel="stylesheet">
+
+    <!--Cargando íconos-->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    
+    <!--Carga de archivos css-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+
+  </head>
+  <body class="paginas-internas">
+    <section class="bienvenidos">
+        <header class="encabezado navbar-fixed-top" rol="banner">
+            <div class="container">
+                <a href="index.php" class="logo">
+                    <img src="images/logo.svg" alt="Logo del sitio">
+                </a>
+                <button type="button" class="boton-buscar" data-toggle="collapse" data-target="#bloque-buscar" aria-expanded="false">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+                <button type="button" class="boton-menu hidden-md-up" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </button>
+                <!--formulario buscar-->
+                <form action="#" id="bloque-buscar" class="collapse">
+                    <div class="contenedor-bloque-buscar">
+                        <input type="text" placeholder="Buscar...">
+                        <input type="submit" value="Buscar">
+                    </div>
+                </form>
+                <!--fin formulario-->
+                <nav id="menu-principal" class="collapse">
+                    <ul>
+                        <li class="active"><a href="index.php">Inicio</a></li>
+                        <li><a href="nosotros.php">Nosotros</a></li>
+                        <li><a href="servicios.php">Servicios</a></li>
+                        <li><a href="contacto.php">Contacto</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <div class="texto-encabezado text-xs-center">
+            <div class="container">
+                <h1 class="display-4">Nosotros</h1>
+                <p>¿Quíenes Somos? y ¿Qué Hacemos?.</p>
+                
+            </div>
+        </div>
+        
+    </section>
+    <section class="ruta">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 text-xs-right">
+                    <a href="index.php">Inicio</a> » Nosotros
+                </div>
+            </div>
+        </div>
+    </section>
+    <main>
+        <div class="container">
+            <div class="row">
+                <article class="col-xs-12 col-md-8">
+                    <h2>Trabajamos para tu éxito</h2>
+                    <p>NETWORK apuesta por la simplicidad, la actualidad y la originalidad. Creemos que la innovación creativa es la única vía para promover y fomentar contenidos de calidad que fortalezcan la conﬁanza de los usuarios en las posibilidades y nuevos usos de los productos digitales.</p>
+                    <p>Nuestro objetivo es conseguir que clientes y usuarios se sientan conectados en el nuevo mundo de la información con servicios y contenidos que no diﬁculten su relación, sino que la simpliﬁquen y la conviertan en un hecho cotidiano, agradable y satisfactorio</p>
+                    <p>Acordeones</p>
+                </article>
+                <aside class="col-xs-12 col-md-4">
+                    <img src="images/nosotros.svg" alt="Nosotros">
+                </aside>
+            </div>
+        </div>
+    </main>
+    
+    
+    
+    <footer class="piedepagina p-y-1 text-xs-center" role="contentinfo">
+        <div class="container">
+            <p>2016 &copy; NETWORK Todos los derechos reservados</p>
+            <ul class="redes-sociales">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </footer>
+    <a href="#" class="ir-arriba">
+        <i class="fa fa-arrow-circle-up" aria-hidden="true"></i>
+    </a>
+    <!--Carga de archivos js-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+  </body>
+</html>
